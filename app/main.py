533f0c4a51752b7c02119eb072a8d0894c588ed4
Janne <Janne>
2022-09-12 00:40:16 -0500

    cambios

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect,render_template, request
 from . import api
-from backend import controller as c
+from . import controller as c
 app = Flask (__name__)
 app.template_folder = "frontend/templates"
 app.static_folder = "frontend/static"
